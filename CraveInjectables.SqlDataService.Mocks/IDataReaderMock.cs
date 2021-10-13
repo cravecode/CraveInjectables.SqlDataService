1ac90d027f2e7ba4ae6bc6a0832b0b8b2adf7928
Cravecode <brian@bdyconsulting.com>
2021-10-13 07:47:11 -0400

    Implementing interface for Reader Mock

---
+++ CraveInjectables.SqlDataService.Mocks/IDataReaderMock.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CraveInjectables.SqlDataService.Mocks
+{
+    public interface IDataReaderMock<out TObject>
+    {
+        IEnumerator<TObject> Enumerator { get; }
+        bool IsClosed { get; }
+    }
+}
